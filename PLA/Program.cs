7f66b35ef802720495dbb833822f7c705ab8f0f2
Kamil <k.fryszkowski.02@gmail.com>
2022-06-14 16:37:57 +0200

    Plakatowanie, etap 1, XV olimpiada

---
+++ PLA/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace PLA
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] buildings = new int[270000];
+            int height, width, n, result = 0, size = 0;
+
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                width = Convert.ToInt32(input[0]);
+                height = Convert.ToInt32(input[1]);
+
+                while ((size > 0) && (buildings[size - 1] > height))
+                {
+                    size--;
+                }
+
+                if ((size == 0) || (buildings[size - 1] < height))
+                {
+                    result++;
+                    buildings[size] = height;
+                    size++;
+
+                }
+            }
+
+            Console.Write(result);
+        }
+
+    }
+
+}
+
